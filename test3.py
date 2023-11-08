c05e9af5d3133159404e02a45a2a49610d57d289
ashxch7 <144280719+ashxch7@users.noreply.github.com>
2023-11-08 12:39:27 -0600

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,15 @@
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+print("1 for Add, 2 for Subtract, 3 for Multiply, 4 for Divide")
+c=int(input("Enter your choice"))
+if (c==1):
+    d=a+b
+elif (c==2):
+    d=a-b
+elif (c==3):
+    d=a*b
+elif (c==4):
+    d=a/b
+else:
+    print("Invalid Input")
+print (d)
