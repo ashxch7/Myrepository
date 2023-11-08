fdeedb7ce36fdb6a066a9ce62b3e28945500128a
ashxch7 <144280719+ashxch7@users.noreply.github.com>
2023-11-08 12:12:50 -0700

    Update test3.py

--- test3.py
+++ test3.py
@@ -2,18 +2,17 @@ a = int(input("Enter a number"))
 b = int(input("Enter another number"))
 print("1 for Add, 2 for Subtract, 3 for Multiply, 4 for Divide")
 c = int(input("Enter your choice"))
-if (c == 1):
+if c == 1:
     d = a + b
     print(d)
-elif (c == 2):
+elif c == 2:
     d = a - b
     print(d)
-elif (c == 3):
+elif c == 3:
     d = a * b
     print(d)
-elif (c == 4):
-    d = float(a / b)
+elif c == 4:
+    d = a / b
     print(d)
 else:
     print("Invalid Input")
-
